412b73574ce6c730760028b8dc3303e5e47d4dd5
sumitjoshi21 <65641783+sumitjoshi21@users.noreply.github.com>
2020-08-17 21:35:31 +0530

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -83,6 +83,6 @@ def index():
         return render_template('index.html')
 #port = int(os.getenv("PORT"))
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
     #app.run(host='0.0.0.0', port=port)
     # app.run(host='127.0.0.1', port=8001, debug=True)
